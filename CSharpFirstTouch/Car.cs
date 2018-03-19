d5587f553bf0cfdb3dda31af3288331c28642500
alexmanul <aleksejs.marcena@gmail.com>
2018-03-19 22:28:45 +0200

    Car example is added

---
+++ CSharpFirstTouch/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpFirstTouch
+{
+    public class Car
+    {
+        public const int Wheels = 4;
+        public float Speed;
+        public bool IsWorking = true;
+
+        public void SetValues(float speed, bool isWorking)
+        {
+            this.Speed = speed;
+            this.IsWorking = isWorking;
+        }
+
+        public void GetValues()
+        {
+            Console.WriteLine("Car speed is: " + this.Speed + " km/h, car is working now: " + this.IsWorking);
+        }
+        
+    }
+}
--- CSharpFirstTouch/MainClass.cs
+++ CSharpFirstTouch/MainClass.cs
@@ -6,10 +6,33 @@ namespace CSharpFirstTouch
    {
         public static void Main(string[] args)
         {
-            HelloWorld.Hello();
-            FirstClass.FirstMethod();
-            MathFunctions.Math();
-            Converting.ConvertingMethod();
+//            HelloWorld.Hello();
+//            FirstClass.FirstMethod();
+//            MathFunctions.Math();
+//            Converting.ConvertingMethod();
+
+            var audi = new Car {Speed = 280};
+            Console.WriteLine(Car.Wheels);
+            Console.WriteLine(audi.Speed);
+            Console.WriteLine(audi.IsWorking);
+            
+            audi.GetValues();
+
+            audi.SetValues(250.5f, isWorking: false);
+            Console.WriteLine(audi.Speed);
+            Console.WriteLine(audi.IsWorking);
+            
+            audi.GetValues();
+            
+            Console.WriteLine("============");
+            
+            var volvo = new Car {Speed = 240};
+            Console.WriteLine(Car.Wheels);
+            Console.WriteLine(volvo.Speed);
+            Console.WriteLine(volvo.IsWorking);
+            
+            volvo.GetValues();
+            
         } 
      }
 }
